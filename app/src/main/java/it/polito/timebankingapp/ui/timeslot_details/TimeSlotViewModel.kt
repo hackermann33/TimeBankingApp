1fc906299623da101e579a644b38fe10a968b7d9
CesettiLorenzo <lorenzo.cesetti.inf@gmail.com>
2022-04-12 16:28:19 +0200

    Changes by fifi9

---
+++ app/src/main/java/it/polito/timebankingapp/ui/timeslot_details/TimeSlotViewModel.kt
@@ -0,0 +1,29 @@
+package it.polito.timebankingapp.ui.timeslot_details
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TimeSlotViewModel : ViewModel() {
+/*
+    private var _counter = 0;
+
+    /*private val _value = MutableLiveData<Int>().apply() {
+         value = _counter
+     }*/
+
+    //val value: LiveData<Int> = _value
+
+    private val _timeSlot
+
+     = MutableLiveData<TimeSlot>().apply {
+        value = "This is a Fragment"
+    }
+
+    val timeSlot: LiveData<TimeSlot> =
+    //val text: LiveData<String> = _text
+
+
+ */
+
+}
