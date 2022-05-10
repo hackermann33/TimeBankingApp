7837178131bf3489d6ffe0363c1d3349991143a9
Filippo Massa <massafilippo97@gmail.com>
2022-05-10 18:40:00 +0200

    Bug fix profilo utente

--- app/src/main/java/it/polito/timebankingapp/ui/profile/showprofile/ShowProfileFragment.kt
+++ app/src/main/java/it/polito/timebankingapp/ui/profile/showprofile/ShowProfileFragment.kt
@@ -52,7 +52,7 @@ class ShowProfileFragment : Fragment(R.layout.fragment_showprofile) {
         val navController = findNavController()
 
 
-        /*vm.fireBaseUser.observe(viewLifecycleOwner){
+        vm.fireBaseUser.observe(viewLifecycleOwner){
             if(it != null) {
                 loggedUser = it
                 usr = vm.user.value!!
@@ -60,7 +60,7 @@ class ShowProfileFragment : Fragment(R.layout.fragment_showprofile) {
             }
             else
                 navController.navigate(R.id.nav_login)
-        }*/
+        }
 
         //loggedUser = usrVm.userProfile.value!!
         //showProfile(view)
