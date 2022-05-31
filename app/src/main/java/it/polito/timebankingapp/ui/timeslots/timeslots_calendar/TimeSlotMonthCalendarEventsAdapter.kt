3ca6c97d1de589c81151ef9e044813136a467a97
Filippo Massa <massafilippo97@gmail.com>
2022-05-31 16:14:13 +0200

    Rollback alla versione precedente

--- app/src/main/java/it/polito/timebankingapp/ui/timeslots/timeslots_calendar/TimeSlotMonthCalendarEventsAdapter.kt
+++ app/src/main/java/it/polito/timebankingapp/ui/timeslots/timeslots_calendar/TimeSlotMonthCalendarEventsAdapter.kt
@@ -1,17 +1,21 @@
 package it.polito.timebankingapp.ui.timeslots.timeslots_calendar
 
+import android.app.AlertDialog
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.chip.Chip
 import it.polito.timebankingapp.R
 import it.polito.timebankingapp.model.timeslot.TimeSlot
+import it.polito.timebankingapp.ui.timeslots.TimeSlotsViewModel
 import java.time.LocalDate
 
 data class Event(val id: String, val ts: TimeSlot, val date: LocalDate, val isOffered: Boolean)
@@ -19,20 +23,10 @@ data class Event(val id: String, val ts: TimeSlot, val date: LocalDate, val isOf
 //          == false --> requested
 
 class TimeSlotMonthCalendarEventsAdapter(/*val vm : TimeSlotsViewModel*/) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    RecyclerView.Adapter<TimeSlotMonthCalendarEventsAdapter.ItemViewHolder>() {
 
     val events = mutableListOf<Event>()
-    private var isEmpty: Boolean = events.isEmpty()
-
     lateinit var context: Context
-
-    private val VIEW_TYPE_EVENT_LIST = 1
-    private val VIEW_TYPE_EMPTY_MESSAGE = 2
-
-    init {
-        if(isEmpty)
-            events.add(Event("", TimeSlot(), LocalDate.now(), false)) //singolo elemento == messaggio di lista vuota
-    }
 
     class ItemViewHolder(private val mainView: View) : RecyclerView.ViewHolder(mainView) {
         private val tsEstimatedTime: TextView = mainView.findViewById(R.id.calendar_item_ts_duration)
@@ -61,46 +55,25 @@ class TimeSlotMonthCalendarEventsAdapter(/*val vm : TimeSlotsViewModel*/) :
         }
     }
 
-    class EmptyItemViewHolder(private val mainView: View ) : RecyclerView.ViewHolder(mainView) {
-        fun bind() {
-            //empty
-        }
-    }
-
-    override fun getItemViewType(position: Int): Int {
-        return if (!isEmpty) {
-            VIEW_TYPE_EVENT_LIST
-        } else {
-            VIEW_TYPE_EMPTY_MESSAGE
-        }
-    }
-
     //inflate the item_layout-based structure inside each ViewHolder
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val vg: View
-        return if (viewType == VIEW_TYPE_EVENT_LIST) {
-            vg = LayoutInflater
-                .from(parent.context)
-                .inflate(R.layout.time_slot_month_calendar_event_item, parent, false)
-            ItemViewHolder(vg)
-        } else { // (viewType == VIEW_TYPE_EMPTY_MESSAGE) {
-            vg = LayoutInflater
-                .from(parent.context)
-                .inflate(R.layout.time_slot_month_calendar_event_empty_item, parent, false)
-            EmptyItemViewHolder(vg)
-        }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val destination =  R.layout.time_slot_month_calendar_event_item
+
+        val vg = LayoutInflater
+            .from(parent.context)
+            .inflate(destination, parent, false) //attachToRoot: take all you measures
+        //but do not attach it immediately to the ViewHolder tree of components (could be a ghost item)
+
+        return ItemViewHolder(vg)
     }
 
-    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
-        when (viewHolder.itemViewType) {
-            VIEW_TYPE_EVENT_LIST-> (viewHolder as ItemViewHolder).bind(events[position], completeTsAction = {
-                Navigation.findNavController(it).navigate(
-                    R.id.action_nav_timeSlotMonthCalendar_to_nav_markTimeSlotAsCompleted,
-                    bundleOf("timeslot" to events[position].ts)
-                )
-            })
-            VIEW_TYPE_EMPTY_MESSAGE -> (viewHolder as EmptyItemViewHolder).bind()
-        }
+    override fun onBindViewHolder(viewHolder: ItemViewHolder, position: Int) {
+        viewHolder.bind(events[position], completeTsAction = {
+            Navigation.findNavController(it).navigate(
+                R.id.action_nav_timeSlotMonthCalendar_to_nav_markTimeSlotAsCompleted,
+                bundleOf("timeslot" to events[position].ts)
+            )
+        })
     }
 
     override fun getItemCount(): Int = events.size
