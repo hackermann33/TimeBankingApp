024b56ba08dc59149ee74929c25eae2328ee10c7
CesettiLorenzo <=>
2022-06-16 19:43:07 +0200

    minor changes

--- app/src/main/java/it/polito/timebankingapp/ui/chats/chat/ChatViewModel.kt
+++ app/src/main/java/it/polito/timebankingapp/ui/chats/chat/ChatViewModel.kt
@@ -273,7 +273,7 @@ class ChatViewModel(application: Application) : AndroidViewModel(application) {
                 /* !!!!tocheck ==> */
                 true //newBalance
             }
-            //todo
+            // commento
             }.addOnSuccessListener {
             reqTsDocs.get().addOnSuccessListener { reqs-> //aggiorno copie di timeSlots all'interno di chats/requests
                 if(it == true) {
